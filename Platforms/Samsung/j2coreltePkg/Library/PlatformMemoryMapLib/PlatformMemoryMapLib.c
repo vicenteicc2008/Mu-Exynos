bbc4687f0fc794940e1d021960629a8ddc06aded
Vicente Cortes <vicenteicc2008@gmail.com>
2023-10-18 14:47:01 -0300

    fb j2corelte

--- Platforms/Samsung/j2coreltePkg/Library/PlatformMemoryMapLib/PlatformMemoryMapLib.c
+++ Platforms/Samsung/j2coreltePkg/Library/PlatformMemoryMapLib/PlatformMemoryMapLib.c
@@ -10,7 +10,7 @@ static ARM_MEMORY_REGION_DESCRIPTOR_EX gDeviceMemoryDescriptorEx[] = {
     {"CPU Vectors",       0x40C40000, 0x00010000, AddMem, SYS_MEM, SYS_MEM_CAP,  BsCode, WRITE_BACK},
     {"HLOS 1",            0x40000000, 0x40000000, AddMem, SYS_MEM, SYS_MEM_CAP, Conv,   WRITE_BACK},
     {"UEFI FD",           0x50000000, 0x00200000, AddMem, SYS_MEM, SYS_MEM_CAP, BsCode, WRITE_BACK},
-    {"Display Reserved",  0x14830000, 0x00800000, AddMem, MEM_RES, SYS_MEM_CAP, Reserv, WRITE_THROUGH_XN},
+    {"Display Reserved",  0x67000000, 0x00800000, AddMem, MEM_RES, SYS_MEM_CAP, Reserv, WRITE_THROUGH_XN},
 
     /* Terminator for MMU */
     {"Terminator", 0, 0, 0, 0, 0, 0, 0}};
