82e73b5112e189c46d1a701bcc744aa404825cf3
Vicente Cortes <vicenteicc2008@gmail.com>
2023-11-07 16:58:51 -0300

    add a BIOS Module Package

---
+++ Silicon/Samsung/VicePkg/Application/ViceBiosApp/ViceBios.c
@@ -0,0 +1,27 @@
+#include <Uefi.h>
+#include <Library/UefiBootServicesTableLib.h>
+
+EFI_STATUS
+EFIAPI
+UefiMain(
+  IN EFI_HANDLE        ImageHandle,
+  IN EFI_SYSTEM_TABLE  *SystemTable
+)
+{
+  EFI_STATUS Status;
+  EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *ConOut;
+
+  // Inicializar la instancia de ConOut para la salida de texto
+  Status = gST->ConOut->ClearScreen(gST->ConOut);
+  if (EFI_ERROR(Status)) {
+    return Status;
+  }
+
+  // Imprimir un mensaje en la pantalla
+  Status = gST->ConOut->OutputString(gST->ConOut, L"Hello, I am Vice from BIOS!\n");
+  if (EFI_ERROR(Status)) {
+    return Status;
+  }
+
+  return EFI_SUCCESS;
+}
